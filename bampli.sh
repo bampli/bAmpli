f08594635789bf8c79fdd88a0f9a493e1058ddfb
Jose Motta <josemotta@bampli.com>
2020-06-01 15:41:56 -0300

    add bampli.sh

--- bampli.sh
+++ bampli.sh
@@ -4,19 +4,24 @@ if [ $#=1 ]; then
   case $1 in
     "start"|"suspend"|"delete")
      for node in node1 node2 node3 node4;do
+        echo "multipass $1 $node"
         multipass $1 $node
      done
      exit 0
     ;;
+    "new")
+     echo "./k3s-setup.sh"
+     ./k3s-setup.sh
+     exit 0
+  ;;
     "renew")
-     for node in node1 node2 node3 node4;do
-        multipass delete $node
-     done
+     ./bampli.sh delete
+     echo "multipass purge"
      multipass purge
-     ./k3s-setup
+     ./bampli.sh new
      exit 0
   ;;
   esac
 fi
-echo "Usage: $0 start|suspend|renew|delete"
+echo "Usage: $0 start|suspend|new|renew|delete"
 exit 1
